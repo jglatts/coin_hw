cd1cb04ae277a2ae67e000826937bd4e8b532dd6
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-09-08 11:25:57 -0400

    Update cis_1057_hw_one.c

--- cis_1057_hw_one.c
+++ cis_1057_hw_one.c
@@ -7,7 +7,6 @@ Author: John Glatts
 
 #include <stdio.h>
 
-
 int main(int argc, char const *argv[])
 {
 
@@ -16,7 +15,7 @@ int main(int argc, char const *argv[])
 	int quarters_remainder, dimes_remainder, nickels_remainder; //changed from double to int, error message using double
 
 	//get input
-	printf("Enter Item Price and Amount Paid\n");
+	printf("\nEnter Item Price and Amount Paid\n");
 	scanf("%d%d", &price, &payment);
 	printf("\nItem Price(in cents): %d\n", price);
 	printf("Amount Paid(in cents): %d\n", payment);
@@ -24,27 +23,27 @@ int main(int argc, char const *argv[])
 	//calculate change
 	change = payment-price;
 	printf("\nChange Required: %d\n", change);	
-	printf("%d cents in coins can be given as:\n", change);
-	
+	printf("\n%d cents in coins can be given as:\n", change);
+
 	// Getting somewhere, the division/remainder combo seems to WORK
 	// tweak the other calcs to get the right numbers yeeo!!! 
 	quarters = change / 25;
 	quarters_remainder = change % 25;
-	printf("\nQuarters: %lf\n", quarters);
+	printf("\tQuarters: %lf\n", quarters);
 
 	//use the remainder to see how many dimes we can use
 	dimes = quarters_remainder / 10;
 	dimes_remainder = quarters_remainder % 10;
-	printf("Dimes: %lf\n", dimes);
+	printf("\tDimes: %lf\n", dimes);
 
 	//use the remainder to see how many nickels we can use
 	nickels = dimes_remainder / 5;
 	nickels_remainder = dimes_remainder % 5;
-	printf("Nickels: %lf\n", nickels);
-	
+	printf("\tNickels: %lf\n", nickels);
+
 	//use the remainder to see how many pennies we can use
 	//no calc needed for pennies
-	printf("Pennies: %d\n", nickels_remainder);
-	
+	printf("\tPennies: %d\n", nickels_remainder);
+		
 	return 0;
 }
